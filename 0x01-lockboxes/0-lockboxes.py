a7ab02805d5673e075940feccce30864f5c1e021
Oluwaseyi Idowu <idowuseyi22@gmail.com>
2023-04-28 05:58:21 +0100

    checks update

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-"""Defines a function that checks if a box containing a list and if the list
-   of lists can be opened using keys stored in the lists.
+"""Defines a function that determines if a box containing a list
+   of lists can be opened using keys stored in the lists
 """
 
 
 def canUnlockAll(boxes):
-    """Checks the boxes and determine the possibility of unlocking the boxes"""
-   positionBox = 0
-    unlockedBox = {}
+    """Determines if boxes can be unlocked"""
+    position = 0
+    unlocked = {}
 
     for box in boxes:
-        if len(box) == 0 or positionBox == 0:
-            unlockedBox[positionBox] = "always_unlocked"
+        if len(box) == 0 or position == 0:
+            unlocked[position] = "always_unlocked"
         for key in box:
-            if key < len(boxes) and key != positionBox:
-                unlockedBox[key] = key
-        if len(unlockedBox) == len(boxes):
+            if key < len(boxes) and key != position:
+                unlocked[key] = key
+        if len(unlocked) == len(boxes):
             return True
-        positionBox += 1
+        position += 1
     return False
